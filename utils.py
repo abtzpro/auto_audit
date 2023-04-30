d50615665b92368b06c96f8195a04d17d813dc4d
Adam R <121298839+abtzpro@users.noreply.github.com>
2023-04-30 08:22:31 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,34 @@
+import smtplib
+from email.message import EmailMessage
+
+def send_email(config, subject, body, attachments=None):
+    # Implement the email sending functionality here using the smtplib library
+    pass
+
+
+import sys
+
+def print_progress(message):
+    sys.stdout.write("\r" + message)
+    sys.stdout.flush()
+
+
+from jinja2 import Environment, FileSystemLoader
+
+def generate_html_report(template_name, data):
+    env = Environment(loader=FileSystemLoader("templates"))
+    template = env.get_template(template_name)
+    return template.render(data=data)
+
+
+def categorize_vulnerabilities(cvss_score):
+    if cvss_score >= 9.0:
+        return "Critical"
+    elif cvss_score >= 4.0:
+        return "Moderate"
+    else:
+        return "Common"
+
+
+
 
